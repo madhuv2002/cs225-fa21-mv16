aa450bb20c083088436776ef34c63c0652bc339e
Madhu Vakkadala <mv16@linux-30.ews.illinois.edu>
2021-08-29 13:54:12 -0500

    Read and write the PNG from the file

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -3,9 +3,23 @@
 
 #include <string>
 
+using cs225::PNG;
+using cs225::HSLAPixel;
 
 void rotate(std::string inputFile, std::string outputFile) {
   // TODO: Part 2
+  PNG inputImage;
+  inputImage.readFromFile(inputFile);
+  PNG outputImage(inputImage.width(), inputImage.height());
+  
+  for (unsigned x = 0; x < inputImage.width(); x++) {
+    for (unsigned y = 0; y < inputImage.height(); y++) {
+      HSLAPixel & pixel1 = inputImage.getPixel(x, y);
+      HSLAPixel & pixel2 = outputImage.getPixel(x, y);
+      pixel2 = pixel1;
+    }
+  }
+  outputImage.writeToFile(outputFile);
 }
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
