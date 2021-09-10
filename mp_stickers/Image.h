f0ad8cd59c1e52126d162767ddf8f80f07e0350a
Madhu Vakkadala <mv16@linux-27.ews.illinois.edu>
2021-09-09 20:26:56 -0500

    Complete image header file with descriptions

--- mp_stickers/Image.h
+++ mp_stickers/Image.h
@@ -34,6 +34,57 @@ namespace cs225 {
      */
     void darken(double amount);
     }
+    
+    /**
+     * Saturates an Image by increasing the saturation
+     * of every pixel by 0.1.
+     */    
+    void saturate();
+    
+    /**
+     * Saturates an Image by increaasing the saturation
+     * of every pixel by amount.
+     */
+    void saturate(double amount);
+    }
+
+    /**
+     * Desaturates an Image by decreasing the saturation
+     * of every pixel by 0.1.
+     */    
+    void desaturate();
+
+    /**
+     * Desaturates an Image by decreaasing the saturation
+     * of every pixel by amount.
+     */
+    void desaturate(double amount);
+    }
+
+    /**
+     * Turns the image grayscale.
+     */  
+    void grayscale();
+
+    /**
+     * Rotates the color wheel by degrees.
+     */
+    void rotateColor(double degrees);
+    
+    /**
+     * Illinify the image.
+     */
+    void illinify();
+    
+    /**
+     * Scale the image by the given factor.
+     */
+    void scale(double factor);
+    
+    /**
+     * Scales the image to fit within the size (w x h).
+     */
+    void scale(unsigned w, unsigned h);
   };
 }
 
