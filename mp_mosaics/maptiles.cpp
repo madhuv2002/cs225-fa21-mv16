1d5c4d5fdee88912e0ecf650c1c90eaaa162bd04
Madhu Vakkadala <mv16@linux-a1.ews.illinois.edu>
2021-11-01 20:48:19 -0500

    Complete mp

--- mp_mosaics/maptiles.cpp
+++ mp_mosaics/maptiles.cpp
@@ -22,6 +22,28 @@ MosaicCanvas* mapTiles(SourceImage const& theSource,
      * @todo Implement this function!
      */
 
-    return NULL;
+    vector<Point<3>> points;
+    map<Point<3>, TileImage*> tiles;
+    
+    for (auto it = theTiles.begin(); it != theTiles.end(); it++) {
+      LUVAPixel pixel = it->getAverageColor();
+      Point<3> point = convertToXYZ(pixel);
+      points.push_back(point);
+      tiles[point] = &*it;
+    }
+    
+    KDTree<3>* tree = new KDTree<3>(points);
+    MosaicCanvas* toReturn = new MosaicCanvas(theSource.getRows(), theSource.getColumns());
+    for (int i = 0; i < theSource.getRows(); i++) {
+      for (int j = 0; j < theSource.getColumns(); j++) {
+        LUVAPixel pixel = theSource.getRegionColor(i, j);
+        Point<3> temp = convertToXYZ(pixel);
+        Point<3> temp2 = tree->findNearestNeighbor(temp);
+        TileImage* tile = tiles[temp2];
+        toReturn->setTile(i, j, tile);
+      }
+    }
+    delete tree;
+    return toReturn;
 }
 
