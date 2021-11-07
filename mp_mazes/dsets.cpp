e1f6b01b22c074322ec68ea283f9129ef77b5395
Madhu Vakkadala <mv16@linux-15.ews.illinois.edu>
2021-11-06 19:21:44 -0500

    Complete part 1

--- mp_mazes/dsets.cpp
+++ mp_mazes/dsets.cpp
@@ -1,1 +1,35 @@
 /* Your code here! */
+#include "dsets.h"
+
+
+void DisjointSets::addelements(int num) {
+  for (int i = 0; i < num; i++) {
+    elems.push_back(-1);
+  }
+}
+
+int DisjointSets::find(int elem) {
+  if (elems[elem] >= 0) {
+    elems[elem] = find(elems[elem]);
+    return elems[elem];
+  }
+  return elem;
+}
+
+void DisjointSets::setunion(int a, int b) {
+  if (find(a) == find(b)) {
+    return;
+  }
+  
+  if (find(a) > find(b)) {
+    elems[find(b)] = elems[find(a)] + elems[find(b)];
+    elems[find(a)] = find(b);
+  } else {
+    elems[find(a)] = elems[find(a)] + elems[find(b)];
+    elems[find(b)] = find(a);
+  }
+}
+
+int DisjointSets::size(int elem) {
+  return elems[find(elem)] * -1;
+}
